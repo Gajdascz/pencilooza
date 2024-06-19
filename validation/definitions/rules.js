e747738d44fd5feaf82e667095f7e895403d8d76
gajdascz <gajdascz@gmail.com>
2024-06-19 16:02:26 -0400

    Fix empty optional fields failing validation

--- validation/definitions/rules.js
+++ validation/definitions/rules.js
@@ -12,60 +12,56 @@ const escapeRemove = {
     options: (str) => str.replace(/&[^\s;]+;/g, ''),
   },
 };
-
-const getOptional = (is = false) => (is ? { options: { nullable: true, checkFalsy: true } } : false);
+const getOptional = (is = false) => (is ? { optional: { options: { nullable: true, checkFalsy: true } } } : {});
 
 // #endregion
 
 // #region Rule Definitions
 const strByLength = (fieldSelector, { min = -Infinity, max = Infinity, optional = false } = {}) => ({
+  ...getOptional(optional),
   trim: true,
   isLength: { options: { min, max } },
-  ...getOptional(optional),
   ...errorMessage(fieldSelector, genericMessages.inRange(min, max, 'characters')),
   ...escapeRemove,
 });
-const strByFormat = (fieldSelector, formatStrRegEx, formatStrExample, { optional = false } = {}) => {
-  const rules = {
-    trim: true,
-    matches: {
-      options: formatStrRegEx,
-      ...errorMessage(fieldSelector, genericMessages.hasFormat(formatStrExample)),
-    },
-    ...escapeRemove,
-  };
-  if (optional) return { optional: { options: { nullable: true, checkFalsy: true } }, ...rules };
-  return rules;
-};
+const strByFormat = (fieldSelector, formatStrRegEx, formatStrExample, { optional = false } = {}) => ({
+  ...getOptional(optional),
+  trim: true,
+  matches: {
+    options: formatStrRegEx,
+    ...errorMessage(fieldSelector, genericMessages.hasFormat(formatStrExample)),
+  },
+  ...escapeRemove,
+});
 const strBySelection = (fieldSelector, providedOptions, { optional = false } = {}) => ({
   ...getOptional(optional),
   isIn: { options: [providedOptions], ...errorMessage(fieldSelector, genericMessages.inProvidedOptions()) },
 });
 const numberByRange = (fieldSelector, { min = -Infinity, max = Infinity, optional = false } = {}) => ({
-  trim: true,
   ...getOptional(optional),
+  trim: true,
   isInt: { options: { min, max } },
   ...errorMessage(fieldSelector, genericMessages.inRange(min, max)),
   ...escapeRemove,
 });
 
 const email = (fieldSelector, { optional = false } = {}) => ({
+  ...getOptional(optional),
   trim: true,
   isEmail: true,
-  ...getOptional(optional),
   ...errorMessage(fieldSelector, genericMessages.hasFormat('name@domain.ext')),
   ...escapeRemove,
 });
 const phone = (fieldSelector, { optional = false } = {}) => ({
-  trim: true,
   ...getOptional(optional),
+  trim: true,
   isLength: { options: { min: 10, max: 12 }, ...errorMessage(fieldSelector, genericMessages.inRange(10, 12)) },
   ...escapeRemove,
 });
 const website = (fieldSelector, { optional = false } = {}) => ({
+  ...getOptional(optional),
   trim: true,
   optional: optional ? { options: { nullable: true, checkFalsy: true } } : false,
-  ...getOptional(optional),
   isURL: {
     options: { protocols: ['https'], require_protocol: true, validate_length: true },
     ...errorMessage(fieldSelector, genericMessages.includes('https protocol and a valid length')),
@@ -73,9 +69,9 @@ const website = (fieldSelector, { optional = false } = {}) => ({
 });
 
 const shorthandReference = (fieldSelector, { min = 3, max = 10, optional = false } = {}) => ({
+  ...getOptional(optional),
   trim: true,
   toUpperCase: true,
-  ...getOptional(optional),
   isLength: {
     options: { min, max },
     ...errorMessage(fieldSelector, genericMessages.inRange(min, max, 'characters')),
@@ -91,17 +87,17 @@ const ein = (fieldSelector, { optional = false } = {}) =>
   strByFormat(fieldSelector, [/^\d{2}-\d{7}$/], 'XX-XXXXXXX', { optional });
 
 const countryCode = (fieldSelector, { optional = false } = {}) => ({
+  ...getOptional(optional),
   trim: true,
   isISO31661Alpha2: true,
-  ...getOptional(optional),
   ...errorMessage(fieldSelector, genericMessages.hasFormat('ISO 031661 Alpha-2, eg. US')),
   ...escapeRemove,
 });
 
 const postalCode = (fieldSelector, { optional = false } = {}) => ({
+  ...getOptional(optional),
   trim: true,
   // express-validator isPostalCode kept throwing error "Invalid locale 'undefined'" despite setting it to 'any'
-  ...getOptional(optional),
   matches: {
     options: [/^[\d-]{3,10}$/],
     ...errorMessage(fieldSelector, genericMessages.inRange(3, 10, 'digits')),
